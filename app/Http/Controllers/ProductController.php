5b034c48d857bf77221171519888d095fdb611f0
Morgane Gougeon <gougeon.morgane@gmail.com>
2021-01-11 15:59:52 +0100

    products page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
 class ProductController extends Controller
 {
     public function index(){
-        return view('product/index');
+        $result = $this->getProductsByPharmacy(1);
+        return view('product/index')->with('products', $result);
+    }
+
+    public function getProductsByPharmacy($id_pharmacy)
+    {    
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('POST', 'https://88-122-235-110.traefik.me:61001/api/product/getProductsByPharmacy', [
+            'verify' => false,
+            'headers' => [
+                'Host' => 'node',
+            ],
+            'form_params' => [
+                'pharmacy_id' => $id_pharmacy,
+            ]
+        ]);
+
+        $resultResponse = json_decode($response->getBody()->getContents());
+
+        if($resultResponse->success){
+            return $resultResponse->result;
+        } else {
+            var_dump($resultResponse->error);
+        }
+
+    }
+
+    public function dropProduct($id_product)
+    {    
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('POST', 'https://88-122-235-110.traefik.me:61001/api/product/deleteProductById', [
+            'verify' => false,
+            'headers' => [
+                'Host' => 'node',
+            ],
+            'form_params' => [
+                'product_id' => $id_product,
+            ]
+        ]);
+
+        $resultResponse = json_decode($response->getBody()->getContents());
+
+        if($resultResponse->success){
+            return $resultResponse->result;
+        } else {
+            var_dump($resultResponse->error);
+        }
+
     }
 }
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,5 +3,38 @@
 @section('title_page', ' Produits')
 
 @section('content')
-    <h3>Produits</h3>
+<div class="card">
+    <div class="card-header card-header-primary">
+        <h3 class="card-title ">Produits</h3>
+         <p class="card-category"> Retrouvez ici l'ensemble des produits proposés sur le shop de votre pharmacie.</p>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table">
+                <thead class=" text-primary">
+                    <th><i class="material-icons">insert_photo</i></th>
+                    <th>Nom</th>
+                    <th>Description</th>
+                    <th>Contenance</th>
+                    <th>Prix</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                        <tr>
+                            <td><img src={{$product->image_url}} style="width:50px;"></td>
+                            <td> {{$product->title}} </td>
+                            <td> {{$product->description}} </td>
+                            <td> {{$product->capacity}} </td>
+                            <td> {{$product->price}} € </td>
+                            <td><i class="material-icons">create</i></td>
+                            <td><i class="material-icons">cancel</i></a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
