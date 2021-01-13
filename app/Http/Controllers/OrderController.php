7845da118aae0994dc319fa748bf8bacdc41fe28
Morgane Gougeon <gougeon.morgane@gmail.com>
2021-01-13 11:54:56 +0100

    orders page

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,9 +5,33 @@ namespace App\Http\Controllers;
 class OrderController extends Controller
 {
     public function index(){
-
-        return view('order/index');
+        $pharmacy = unserialize(session('pharmacy'));
+        $pharmacy_id = $pharmacy->id;
+        $result = $this->getOrdersByPharmacy($pharmacy_id);
+        return view('order/index')->with('orders', $result);
     }
 
-    
+    public function getOrdersByPharmacy($id_pharmacy)
+    {    
+        $client = new \GuzzleHttp\Client();
+
+        $response = $client->request('POST',  env('HOST_URL').env('GET_ORDERS_BY_PHARMACY') , [
+            'verify' => false,
+            'headers' => [
+                'Host' => 'node',
+            ],
+            'form_params' => [
+                'pharmacy_id' => $id_pharmacy,
+            ]
+        ]);
+
+        $resultResponse = json_decode($response->getBody()->getContents());
+
+        if($resultResponse->success){
+            return $resultResponse->result;
+        } else {
+            var_dump($resultResponse->error);
+        }
+
+    }
 }
--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -3,5 +3,83 @@
 @section('title_page', ' Commandes')
 
 @section('content')
-<h3> Commandes</h3>
+<div class="card">
+    <div class="card-header card-header-primary">
+        <h3 class="card-title ">Commandes</h3>
+        <p class="card-category"> Retrouvez l'ensemble des commandes passées sur le shop de votre pharmacie.</p>                 
+    </div>
+    <div class="card-body">
+    <input type="text" id="search_input" class="form-control" onkeyup="searchFunction()" placeholder="Rechercher une commande...">
+    <br><br>
+        <div class="table-responsive">
+            <table class="table" >
+                <thead class=" text-primary">
+                    <th>Id</th>
+                    <th>Statut</th>
+                    <th>Client</th>
+                    <th>Prix</th>
+                    <th>Preparateur</th>
+                    <th>Casier</th>
+                    <th>Date</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+                </thead>
+                <tbody id="orders_table">
+                    @foreach($orders as $order)
+                        <tr>
+                            <td> {{$order->id}} </td>
+                            <td> {{$order->status}} </td>
+                            <td> {{$order->id_client}} </td>
+                            <td> {{$order->total_price}} €</td>
+                            <td> {{$order->id_preparator}} </td>
+                            <td> {{$order->id_container}} </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    function searchFunction() 
+    {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("search_input");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("orders_table");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) 
+        {
+            var cpt = 0;
+
+            for(j=0; j< tr[i].getElementsByTagName("td").length; j++)
+            {
+                td = tr[i].getElementsByTagName("td")[j];
+                if (td) 
+                {
+                    txtValue = td.textContent || td.innerText;
+
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) 
+                    {
+                       cpt= cpt+1;
+                    } 
+                    
+                }  
+            }
+            if (cpt > 0) 
+            {
+                tr[i].style.display = "";
+            } 
+            else 
+            {
+                tr[i].style.display = "none";
+            }    
+        }
+    }
+</script>
 @endsection
