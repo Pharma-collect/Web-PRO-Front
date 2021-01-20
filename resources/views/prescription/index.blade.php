2d7116c24fc06c9a1b43ca323452bdeddb9ab4e2
Morgane Gougeon <gougeon.morgane@gmail.com>
2021-01-20 14:30:29 +0100

    Historique des prescriptions

--- resources/views/prescription/index.blade.php
+++ resources/views/prescription/index.blade.php
@@ -19,6 +19,7 @@
                     <th>Statut</th>
                     <th>Client</th>
                     <th>Commentaires</th>
+                    <th>Date</th>
                     <th>Modifier</th>
                     <th>Supprimer</th>
                 </thead>
@@ -46,6 +47,11 @@
                         @endforeach
                         <td> {{$cli}} </td>
                         <td> {{$prescription->detail}}</td>
+                        @php
+                            $date = $prescription->creation_date;
+                            $parsed_date = date('d-m-Y',strtotime($date));
+                        @endphp
+                        <td> {{$parsed_date}} </td>
                         <td>
                             <a onClick="" href="#">
                                 <form id="" method="post" action="">
@@ -72,6 +78,52 @@
         </div>
     </div>
 </div>
+<br>
+<div class="card">
+    <div class="card-header card-header-primary">
+        <h3 class="card-title ">Historique des prescriptions</h3>
+        <p class="card-category"> Retrouvez l'historique de l'ensemble des prescriptions addressées à votre pharmacie.</p>
+    </div>
+    <div class="card-body">
+    <input type="text" id="search_input_2" class="form-control" onkeyup="searchFunction2()" placeholder="Rechercher une prescription...">
+    <br><br>
+        <div class="table-responsive">
+            <table class="table" >
+                <thead class=" text-primary">
+                    <th>Id</th>
+                    <th>Statut</th>
+                    <th>Client</th>
+                    <th>Commentaires</th>
+                    <th>Date</th>
+                </thead>
+                <tbody id="old_prescriptions_table">
+                    @foreach($prescriptions as $prescription)
+                    @if($prescription->status != "pending")                               
+                    <tr>
+                        <td> {{$prescription->id}} </td>
+                        <td> {{$prescription->status}} </td>
+                        @foreach($clients as $client)
+                            @if($client->id === $prescription->id_client)
+                                @php
+                                    $cli = $client->username     
+                                @endphp 
+                            @endif 
+                        @endforeach
+                        <td> {{$cli}} </td>
+                        <td> {{$prescription->detail}}</td>
+                        @php
+                            $date = $prescription->creation_date;
+                            $parsed_date = date('d-m-Y',strtotime($date));
+                        @endphp
+                        <td> {{$parsed_date}} </td>
+                    </tr>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
 <script>
@@ -82,6 +134,44 @@
         input = document.getElementById("search_input");
         filter = input.value.toUpperCase();
         table = document.getElementById("prescriptions_table");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) 
+        {
+            var cpt = 0;
+
+            for(j=0; j< tr[i].getElementsByTagName("td").length; j++)
+            {
+                td = tr[i].getElementsByTagName("td")[j];
+                if (td) 
+                {
+                    txtValue = td.textContent || td.innerText;
+
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) 
+                    {
+                       cpt= cpt+1;
+                    } 
+                    
+                }  
+            }
+            if (cpt > 0) 
+            {
+                tr[i].style.display = "";
+            } 
+            else 
+            {
+                tr[i].style.display = "none";
+            }    
+        }
+    }
+    function searchFunction2() 
+    {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("search_input_2");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("old_prescriptions_table");
         tr = table.getElementsByTagName("tr");
 
         // Loop through all table rows, and hide those who don't match the search query
