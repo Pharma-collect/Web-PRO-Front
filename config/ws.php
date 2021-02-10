e17a75d15a2857348d33900fb4487287aa541ea2
Lilian Giraudo <lilian.giraudo@gmail.com>
2021-02-10 18:08:27 +0100

    mv .env to config

---
+++ config/ws.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        "DELETE_PRODUCT_BY_ID" =>'/product/deleteProduct',
+        "GET_PRODUCTS_BY_PHARMACY" => '/product/getProductsByPharmacy',
+        "GET_PRODUCT_BY_ID" => '/product/getProductById',
+        "UPDATE_PRODUCT_BY_ID" => '/product/updateProduct',
+        "ADD_PRODUCT" => '/product/createProduct',
+        "LOGIN" => '/user_pro/loginPro',
+    ];
+
